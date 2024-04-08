a07824213e9de65473a375b18c594a2769b25481
Chimwemwe Masona <justplainrodney@gmail.com>
2024-04-08 11:00:15 +0200

    started work on the header component of the homepage

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import Header from "@/components/homepage_sections/Header";
 
 const Homepage = () => {
   return (
     <div className="min-h-screen flex flex-col">
       <Navbar />
       <div className="flex-1 flex justify-center items-center font-bold text-3xl">
-        Homepage
+        <Header />
       </div>
       <Footer />
     </div>
