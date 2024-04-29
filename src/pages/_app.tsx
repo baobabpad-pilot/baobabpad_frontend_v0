333d832180e2d112eb5c1caf3b198d52500bba3d
Crispin Manda <crispinmanda06@gmail.com.com>
2024-04-29 14:46:13 +0300

    solved the merge conflict

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,6 @@
 import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
 import { Session } from "next-auth";
-import { SessionProvider } from "next-auth/react";
 
 export default function App({ Component, pageProps }: AppProps<{ session: Session }>) {
   return (
