60ca97b6dfed5feecf4297f4a0b378c174f652cc
Crispin Manda <crispinmanda06@gmail.com.com>
2024-05-08 12:44:14 +0300

    removed the redux form handling part on store and Auth-api

--- src/redux/slices/Auth-api.tsx
+++ src/redux/slices/Auth-api.tsx
@@ -1,25 +1,25 @@
-// authApi.ts
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+// // authApi.ts
+// import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-export const authApi = createApi({
-  reducerPath: "authApi",
-  baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_BACKEND_URL }),
-  endpoints: (builder) => ({
-    signup: builder.mutation({
-      query: (formData) => ({
-        url: "register",
-        method: "POST",
-        body: formData,
-      }),
-    }),
-    signin: builder.mutation({
-      query: (formData) => ({
-        url: "login",
-        method: "POST",
-        body: formData,
-      }),
-    }),
-  }),
-});
+// export const authApi = createApi({
+//   reducerPath: "authApi",
+//   baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_BACKEND_URL }),
+//   endpoints: (builder) => ({
+//     signup: builder.mutation({
+//       query: (formData) => ({
+//         url: "register",
+//         method: "POST",
+//         body: formData,
+//       }),
+//     }),
+//     signin: builder.mutation({
+//       query: (formData) => ({
+//         url: "login",
+//         method: "POST",
+//         body: formData,
+//       }),
+//     }),
+//   }),
+// });
 
-export const { useSignupMutation, useSigninMutation } = authApi;
+// export const { useSignupMutation, useSigninMutation } = authApi;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,16 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
-import rootReducer, { RootState } from "./rootReducer";
+import rootReducer from "./rootReducer";
 import { setupListeners } from "@reduxjs/toolkit/query";
-import { authApi } from "./slices/Auth-api"; // Import your RTK Query API slice
 
 const store = configureStore({
-  reducer: {
-    ...rootReducer,
-    [authApi.reducerPath]: authApi.reducer,
-  },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(authApi.middleware),
+  reducer: rootReducer,
 });
+
 setupListeners(store.dispatch);
 
 export default store;
