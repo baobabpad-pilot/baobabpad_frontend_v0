cce1fd8f091eba78b78087d6bb3a105f56e208e1
Stephen Ondieki <stevehotcodes@gmail.com>
2024-05-20 14:34:56 +0300

    contmet for AI soltions page

--- src/components/ai_and_data_sections/Large_Language_Modelling.tsx
+++ src/components/ai_and_data_sections/Large_Language_Modelling.tsx
@@ -8,11 +8,18 @@ const Large_Language_Modelling = () => {
       <p>Integrating Generative AI ~AI21 Labs’ models for businesses can boost SEO quality and efficiency by optimizing page rank on the first page when you search for something related to hotels, places, and all-things-travel and ensuring a promising path forward for online success in addressing specific user queries and providing useful information. </p>
 
       <p>
+      AI in Software Solutions: The page discusses the role of an AI Engineer at Silo AI, focusing on building AI-powered software solutions and consulting in various domains including full-stack development, data pipelines, MLOps, and DevOps.
         
       </p>
+      
+      <p>
+      Machine Learning Paradigm: It highlights the paradigm shift brought by machine learning, extending beyond software products, and emphasizes the importance of MLOps for reproducible and monitorable AI solutions.
 
+      </p>
 
+      <p>MLOps Lifecycle: The document outlines the initial and production setups for MLOps, stressing the need for automation in analysis, training, versioning, serving, and monitoring of machine learning models.</p>
 
+     <p>Getting Started with AI: For beginners, it suggests starting with machine learning use-cases on Kaggle, exploring tutorials and models on Hugging Face, and utilizing free GPU resources like Google </p>
 
 
       </div>
--- src/components/ai_and_data_sections/Machine_Learning_Ops.tsx
+++ src/components/ai_and_data_sections/Machine_Learning_Ops.tsx
@@ -2,7 +2,22 @@ import React from 'react'
 
 const Machine_Learning_Ops = () => {
   return (
-    <div>Machine_Learning_Ops</div>
+    <div className="px-4 py-4 flex flex-col gap-8 bg-[url('/introduction-solution-image.png')] bg-cover bg-center relative text-white bg-white bg-opacity-50 backdrop-blur-5 text-base">
+    <div className="absolute top-0 left-0 right-0 bottom-0 bg-gray-200 opacity-30 z-10"></div>
+    <div className="bg-black  bg-opacity-75 p-4 rounded-lg">
+      <p>AI in Software Solutions: The page discusses building AI-powered software solutions, with a focus on full-stack development, data pipelines, MLOps, and DevOps.</p>
+
+      <p>Machine Learning Paradigm: It highlights the paradigm shift machine learning represents and its application beyond software products.</p>
+
+      <p>MLOps Importance: Emphasizes the role of MLOps in ensuring AI project success, from initial manual setups to automated ML pipelines for analysis, training, and serving.</p>
+
+      <p>Foundation Models: The page touches on the era of foundation models, their faster time to value, and the challenges in objectively measuring output quality.</p>
+
+
+
+
+      </div>
+      </div>
   )
 }
 
